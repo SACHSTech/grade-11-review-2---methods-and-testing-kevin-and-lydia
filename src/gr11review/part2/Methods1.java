816b3bbe994da7f20a92f27fcd43db0b6722d1e3
iydia <lydia.he23@ycdsbk12.ca>
2022-09-26 10:57:34 -0400

    updated methods1. still doesnt work

--- src/gr11review/part2/Methods1.java
+++ src/gr11review/part2/Methods1.java
@@ -1,5 +1,4 @@
 package gr11review.part2;
-import java.io.*;
 
 /**
  * Author: Lydia He
@@ -8,41 +7,37 @@ import java.io.*;
 
 public class Methods1 {
     // Variables
-    static int intCharX = 0;
-    static int intCharY = 0;
+    static int intCharX = -1;
+    static int intCharY = -1;
     static boolean blnHasX = false;
     static boolean blnHasY = false;
 
     public static boolean xyBalance(String str) {
-    
         // String Methods
         for (int i = 0; i < str.length(); i++) {
-            // Checks for letter "x"
             if (str.charAt(i) == 'x') {
                 intCharX = i;
-                blnHasX = true;
             }
 
-            // Checks for letter "y"
             if (str.charAt(i) == 'y') {
                 intCharY = i;
-                blnHasY = true;
             }
         }
 
-        if ((blnHasX == true) && (blnHasY == true)) {
-            if (intCharY > intCharX) {
-                return true;
-            }
+        if (intCharY > intCharX) {
+            return true;
         } else {
             return false;
         }
     }
 
+
     public static void main(String[] args) {
-        System.out.println(xyBalance("aaxbby"));
-        System.out.println(xyBalance("aaxbb"));
-        System.out.println(xyBalance("yaaxbb"));
+        System.out.println(xyBalance("aaxbby")); // true
+        System.out.println(xyBalance("aaxbb")); // false
+        System.out.println(xyBalance("yaaxbb")); // false
+        System.out.println(xyBalance("xxyxxy")); // true
+        System.out.println(xyBalance("yxxyxx")); // false
     }
 }
     
