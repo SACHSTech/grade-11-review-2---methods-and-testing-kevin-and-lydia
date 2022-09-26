d95845e0b162eaff3c7875773e10625dc2aedd6b
iydia <lydia.he23@ycdsbk12.ca>
2022-09-26 10:40:30 -0400

    updated methods1.java

--- src/gr11review/part2/Methods1.java
+++ src/gr11review/part2/Methods1.java
@@ -3,37 +3,46 @@ import java.io.*;
 
 /**
  * Author: Lydia He
- * Description: This program reutrns true if the given string is xy-balanced
+ * Description: This program reutrns true if the given string is xy-balanced. 
  */
 
 public class Methods1 {
-    public static void main(String[] args) {
-        // Variables
-        int intCharX;
-        int intCharY;
-    
-        public static boolean xyBalance(String str) {
-        
-            // String Methods
-            for (int i = 0; i < str.length(); i++) {
-                // Checks for letter "x"
-                if (str.charAt(i) == 'x') {
-                    intCharX = i;
-                }
+    // Variables
+    static int intCharX = 0;
+    static int intCharY = 0;
+    static boolean blnHasX = false;
+    static boolean blnHasY = false;
 
-                // Checks for letter "y"
-                if (str.charAt(i) == 'y') {
-                    intCharY = i;
-                }
+    public static boolean xyBalance(String str) {
+    
+        // String Methods
+        for (int i = 0; i < str.length(); i++) {
+            // Checks for letter "x"
+            if (str.charAt(i) == 'x') {
+                intCharX = i;
+                blnHasX = true;
             }
 
+            // Checks for letter "y"
+            if (str.charAt(i) == 'y') {
+                intCharY = i;
+                blnHasY = true;
+            }
+        }
+
+        if ((blnHasX == true) && (blnHasY == true)) {
             if (intCharY > intCharX) {
                 return true;
-            } else {
-                return false;
             }
-            
+        } else {
+            return false;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(xyBalance("aaxbby"));
+        System.out.println(xyBalance("aaxbb"));
+        System.out.println(xyBalance("yaaxbb"));
     }
 }
     
