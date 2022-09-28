f618d4944070e4d33bb28d241b6ebf080f91f2c0
iydia <lydia.he23@ycdsbk12.ca>
2022-09-28 09:53:18 -0400

    Began fileIO1

---
+++ src/gr11review/part2/FileIO1.java
@@ -0,0 +1,19 @@
+package gr11review.part2;
+
+public class FileIO1 {
+
+    /**
+     * Author: Lydia He
+     * Description: This program returns the longest word in the file,
+     * given the name of a file filenametxt that contains a single word on each line.
+     * @param filenametxt a file that contains a single word on each line.
+     * @return returns the longest word in the file.
+     */
+    public static String longestWord(String filenametxt) {
+
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
